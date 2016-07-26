813e760b43f637971e63863e5149a92658a9f871
Jason Dean <jdean@feature23.com>
2016-07-26 14:03:00 -0400

    Add ability to select from multiple logs (azure tables)

--- AzureSLABViewer/AzureSLABViewer.Web/Controllers/LogController.cs
+++ AzureSLABViewer/AzureSLABViewer.Web/Controllers/LogController.cs
@@ -5,6 +5,9 @@ using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage.Table.Queryable;
 using System.Linq;
 using System.Web.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Web;
 
 namespace AzureSLABViewer.Web.Controllers
 {
@@ -12,18 +15,29 @@ namespace AzureSLABViewer.Web.Controllers
     {
         private static readonly string _storageConnection = ConfigurationManager.AppSettings["StorageConnection"] ?? "UseDevelopmentStorage=true";
         private static readonly string _siteTitle = ConfigurationManager.AppSettings["SiteTitle"] ?? "Azure SLAB Logs";
+        private CloudStorageAccount _storageAccount;
+        private CloudTableClient _tableClient;
+        private const string cookieName = "SelectedSLABLogTable";
+        private HttpCookie _slabViewerCookie;
+
+        public LogController()
+        {
+            _storageAccount = CloudStorageAccount.Parse(_storageConnection);
+            _tableClient = _storageAccount.CreateCloudTableClient();
+        }
 
         public ActionResult Details(string pk, string rk)
         {
-            if (string.IsNullOrEmpty(pk)
-                || string.IsNullOrEmpty(rk))
+            _slabViewerCookie = Request.Cookies[cookieName];
+
+            if (string.IsNullOrEmpty(pk) ||
+                string.IsNullOrEmpty(rk) ||
+                _slabViewerCookie == null)
+            {
                 return Redirect("/");
-
-            var storageAccount = CloudStorageAccount.Parse(_storageConnection);
-
-            var tableClient = storageAccount.CreateCloudTableClient();
-
-            var table = tableClient.GetTableReference("SlabLogsTable");
+            }
+            
+            var table = _tableClient.GetTableReference(_slabViewerCookie.Value);
 
             if (!table.Exists())
                 return HttpNotFound();
@@ -42,41 +56,76 @@ namespace AzureSLABViewer.Web.Controllers
 
         public ActionResult List(string pk, string rk)
         {
-            var storageAccount = CloudStorageAccount.Parse(_storageConnection);
+            var selectedSlabLog = string.Empty;
 
-            var tableClient = storageAccount.CreateCloudTableClient();
-
-            var table = tableClient.GetTableReference("SlabLogsTable");
-
-            if (!table.Exists())
-                return HttpNotFound();
-
-            var query = (from ent in table.CreateQuery<SlabLogsTable>()
-                         select ent)
-                         .Take(100)
-                         .AsTableQuery();
-
-            TableContinuationToken token = null;
-
-            if (!string.IsNullOrEmpty(pk) && !string.IsNullOrEmpty(rk))
-                token = new TableContinuationToken() { NextPartitionKey = pk, NextRowKey = rk };
-
-            var queryResult = query.ExecuteSegmented(token);
-
-            var results = queryResult
-                            .ToList()
-                            .OrderByDescending(i => i.EventDate); // added to handle issue when rowkey not deterministic across apps
-
-            if (queryResult.ContinuationToken != null)
+            _slabViewerCookie = Request.Cookies[cookieName];
+            
+            if (_slabViewerCookie != null)
             {
-                ViewBag.HasNextPage = true;
-                ViewBag.NextPartitionKey = queryResult.ContinuationToken.NextPartitionKey;
-                ViewBag.NextRowKey = queryResult.ContinuationToken.NextRowKey;
+                selectedSlabLog = _slabViewerCookie.Value;
+            }
+
+            var tables = _tableClient.ListTables()
+                                            .Where(t => t.Name.ToLower().Contains("logstable"))
+                                            .OrderBy(t => t.Name);
+
+            var vm = SLABLogListViewModel.Create(new List<SlabLogsTable>(), tables, selectedSlabLog);
+        
+            if (string.IsNullOrEmpty(selectedSlabLog))
+            {
+                ViewBag.HasNextPage = false;
+                return View(vm);
             }
             else
-                ViewBag.HasNextPage = false;
+            {
+                var table = _tableClient.GetTableReference(selectedSlabLog);
 
-            return View(results);
+                if (!table.Exists())
+                    return HttpNotFound();
+
+                var query = (from ent in table.CreateQuery<SlabLogsTable>()
+                             select ent)
+                             .Take(100)
+                             .AsTableQuery();
+
+                TableContinuationToken token = null;
+
+                if (!string.IsNullOrEmpty(pk) && !string.IsNullOrEmpty(rk))
+                    token = new TableContinuationToken() { NextPartitionKey = pk, NextRowKey = rk };
+
+                var queryResult = query.ExecuteSegmented(token);
+
+                var results = queryResult
+                                .ToList()
+                                .OrderByDescending(i => i.EventDate); // added to handle issue when rowkey not deterministic across apps
+
+                if (queryResult.ContinuationToken != null)
+                {
+                    ViewBag.HasNextPage = true;
+                    ViewBag.NextPartitionKey = queryResult.ContinuationToken.NextPartitionKey;
+                    ViewBag.NextRowKey = queryResult.ContinuationToken.NextRowKey;
+                }
+                else
+                    ViewBag.HasNextPage = false;
+
+                vm.SLABLogItems = results;
+
+                return View(vm);
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult List(string selectedSLABLogTable)
+        {
+            var slabViewerCookie = new HttpCookie(cookieName);
+            slabViewerCookie.Value = selectedSLABLogTable;
+            slabViewerCookie.Expires = DateTime.Now.AddYears(1); //whenever...
+
+            Response.Cookies.Add(slabViewerCookie);
+
+            return RedirectToAction("List");
         }
 
         protected override ViewResult View(string viewName, string masterName, object model)
---
+++ AzureSLABViewer/AzureSLABViewer.Web/Models/SLABLogListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureSLABViewer.Web.Models
+{
+    public class SLABLogListViewModel
+    {
+        public string SelectedSLABLogTable { get; set; }
+
+        public IEnumerable<SlabLogsTable> SLABLogItems {get; set;}
+
+        public IEnumerable<CloudTable> SLABLogTables { get; set; }
+
+        public static SLABLogListViewModel Create(IEnumerable<SlabLogsTable> slabLogItems,
+                                           IEnumerable<CloudTable> slabLogTables,
+                                           string selectedSLABLogTable)
+        {
+            return new SLABLogListViewModel()
+            {
+                SLABLogItems = slabLogItems,
+                SLABLogTables = slabLogTables,
+                SelectedSLABLogTable = selectedSLABLogTable
+            };
+        }
+    }
+}
